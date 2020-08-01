40de5ec96e4705202ec3d613d87b6426412cb85c
Lakin Wecker <lakin@structuredabstraction.com>
2020-08-01 09:00:52 -0600

    Some changes for sebbers

---
+++ bin/bin/xrandr-sebbers-home-desk.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+xrandr
+xrandr --auto --output eDP1 --mode 1920x1080 --dpi 94
+xrandr --auto --output eDP1 --mode 1920x1080 --same-as DP3
+xrandr --auto --output DP1 --mode 3840x2160 --right-of eDP1
+gsettings set org.gnome.desktop.interface scaling-factor 2
--- setup/arch-devel-packages.sh
+++ setup/arch-devel-packages.sh
@@ -7,13 +7,12 @@ trizen -Syu --needed clang clang-tools-extra cmake rsync gdb gcc \
 
 # Jabba for java
 curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash && . ~/.jabba/jabba.sh
-jabba install openjdk@1.14.0-32
-jabba install openjdk@1.13.0-2
+jabba install openjdk@1.14.0-1
 # nvm for node
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
-nvm install 12
-nvm use 12
+#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
+#nvm install 12
+#nvm use 12
 # Rustup for rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-curl -Lo cs https://git.io/coursier-cli-linux && chmod +x cs && mv cs ~/gbin/ && ./cs --help
-/home/lakin/gbin/cs bootstrap  --java-opt -Xss4m --java-opt -Xms100m --java-opt -Dmetals.client=vim-lsc org.scalameta:metals_2.12:0.8.1 -r bintray:scalacenter/releases -r sonatype:snapshots -o /home/lakin/gbin/metals-vim -f
+#curl -Lo cs https://git.io/coursier-cli-linux && chmod +x cs && mv cs ~/gbin/ && ~/gbin/cs --help
+#/home/lakin/gbin/cs bootstrap  --java-opt -Xss4m --java-opt -Xms100m --java-opt -Dmetals.client=vim-lsc org.scalameta:metals_2.12:0.8.1 -r bintray:scalacenter/releases -r sonatype:snapshots -o /home/lakin/gbin/metals-vim -f
