b99c79dd9bfca0ae6c172d890dd10c155d59b159
Lakin Wecker <lakin@structuredabstraction.com>
2022-01-03 19:59:01 -0700

    Properly setup scala metals with my keybindings.

--- nvim-lua/.config/nvim/lua/eldub/lspconfig.lua
+++ nvim-lua/.config/nvim/lua/eldub/lspconfig.lua
@@ -23,32 +23,14 @@ function M.init(use)
             requested_server:on_ready(function ()
               local on_attach = function(_, bufnr)
                 local map = require("eldub.keys")
+                local ide = require("eldub.ide")
                 local n = function(...)
                   map.buf_n(bufnr, ...)
                 end
                 -- Enable completion triggered by <c-x><c-o>
                 vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+                ide.init(n)
 
-                n("[c", '<cmd>lua vim.diagnostic.goto_prev()<CR>')
-                n("]c", '<cmd>lua vim.diagnostic.goto_next()<CR>')
-                n("K", '<cmd>lua vim.lsp.buf.hover()<CR>')
-                n("<leader>br", '<cmd>lua vim.lsp.buf.references()<CR>')
-                n('<space>?', '<cmd>lua vim.diagnostic.open_float()<CR>')
-                n('<leader>bD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
-                n('<leader>bd', '<cmd>lua vim.lsp.buf.definition()<CR>')
-                n('<leader>bi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
-                n('<leader>bt', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
-                n('<leader>bk', '<cmd>lua vim.lsp.buf.signature_help()<CR>')
-                n('<leader>ba', '<cmd>lua vim.lsp.buf.code_action()<CR>')
-                n('<leader>bR', '<cmd>lua vim.lsp.buf.rename()<CR>')
-                n('<leader>F', '<cmd>lua vim.lsp.buf.formatting()<CR>')
-
-                -- See `:help vim.lsp.*` for documentation on any of the below functions
-                --n('<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>')
-                --n('<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>')
-                --n('<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>')
-                --n('<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>')
-                --n('<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>')
               end
               local opts = {
                 on_attach = on_attach,
--- nvim-lua/.config/nvim/lua/eldub/metals.lua
+++ nvim-lua/.config/nvim/lua/eldub/metals.lua
@@ -1,19 +1,24 @@
 M = {}
 function M.init_scala()
-  print("---------------------------")
   local metals_config = require("metals").bare_config()
   metals_config.init_options.statusBarProvider = "on"
   require("metals").initialize_or_attach(metals_config)
+  local ide = require("eldub.ide")
+  local map = require("eldub.keys")
+  local n = function(...)
+    map.n( ...)
+  end
+
+  ide.init(n)
 end
 function M.init(use)
   use {
     'scalameta/nvim-metals',
     requires = { "nvim-lua/plenary.nvim" },
     config = function()
-      print("--------------------------------------------")
       vim.cmd [[augroup metals]]
       vim.cmd [[au!]]
-      vim.cmd [[au FileType java,scala,sbt lua require("eldub.scala").init_scala()]]
+      vim.cmd [[au FileType java,scala,sbt lua require("eldub.metals").init_scala()]]
       vim.cmd [[augroup end]]
     end
   }
