2e8b68beab14bb7f0f24a026738bb0a55be14b3e
Lakin Wecker <lakin@structuredabstraction.com>
2022-01-08 19:01:40 -0700

    Fixing config for intel only again

--- bin/bin/xrandr-sebbers.sh
+++ bin/bin/xrandr-sebbers.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 xrandr
-xrandr --auto --output eDP-1 --mode 1920x1080
-xrandr --auto --output DP-1 --off
+xrandr --auto --output eDP1 --mode 1920x1080
+xrandr --auto --output DP3 --off
 #xrandr --auto --output DP-1 --mode 2560x1440 --left-of eDP-1 --dpi 96
 xrandr --auto --output VIRTUAL-1 --off
 xrandr --auto --output HDM-1 --off
 xrandr --auto --output HDM-2 --off
 xrandr --auto --output VIRTUAL-1 --off
 gsettings set org.gnome.desktop.interface scaling-factor 2
+~/bin/choose-wallpapers.py ~/wallpapers/dracula/arch.png && ~/bin/wallpaper.py
+~/bin/polybar.sh
