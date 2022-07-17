c0a3244e9dcbaa2c9cf042da49ce0a862888300d
Lakin Wecker <lakin@structuredabstraction.com>
2022-07-17 13:05:59 -0600

    hover

---
+++ nvim-lua/.config/nvim/lua/eldub/hover.lua
@@ -0,0 +1,28 @@
+M = {}
+function M.init(use)
+  use{
+    'lewis6991/hover.nvim',
+    config = function()
+      require('hover').setup{
+        init = function()
+          -- Require providers
+          require('hover.providers.lsp')
+          -- require('hover.providers.gh')
+          -- require('hover.providers.man')
+          -- require('hover.providers.dictionary')
+        end,
+        preview_opts = {
+          border = 'single'
+        },
+        title = true
+      }
+
+      -- Setup keymaps
+      --vim.keymap.set('n', 'lK', require('hover').hover, { desc='hover.nvim'         })
+      --vim.keymap.set('n', 'gK', require('hover').hover_select, { desc='hover.nvim (select)' })
+    end
+  }
+end
+
+
+return M
